f85711a903a83e4473329aac747146efbb61a4c6
Vilas Boas <Vilas Boas@DESKTOP-33BU23A>
2020-09-28 20:24:58 -0300

    deletar classe n usada

--- Ativ4/src/br/up/dominio/ItensPedido.java
+++
@@ -1,9 +0,0 @@
-package br.up.dominio;
-
-public class ItensPedido {
-	String observacao = new String(" ");
-	String prato = new String(" ");
-	String vinho = new String(" ");
-	String bebida = new String(" ");
-	double preço = 0;
-}
